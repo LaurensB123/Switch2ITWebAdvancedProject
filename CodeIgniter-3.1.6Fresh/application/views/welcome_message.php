c3fb4ae0191a52845d6f78fd3cb4b65e929f0b0c
hannahpatronoudis <hannah.patronoudis@student.pxl.be>
2017-10-29 17:52:30 +0100

    Bootstrap gefixt
    Bootstrap gefixt o welkom pagina

--- CodeIgniter-3.1.6Fresh/application/views/welcome_message.php
+++ CodeIgniter-3.1.6Fresh/application/views/welcome_message.php
@@ -1,53 +1,50 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+    <title>Welcome to CodeIgniter</title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
     <link href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/HomeStyle.css">
-    <meta charset="utf-8">
-    <title>Welcome to CodeIgniter</title>
-
-
 </head>
+
 <body>
 <div class="container">
-<div id="pagegradient">
     <div class="row">
         <div class="col-md-3">
-    <a class="button" href="<?php echo site_url('Welcome/loadContactenAanmaken/uri') ?>">
-        <span>Contact aanmaken</span>
-    </a>
+            <p><a class="button" href="<?php echo site_url('Welcome/loadContactenAanmaken/uri') ?>">
+                    <span>Contact aanmaken</span>
+                </a></p>
         </div>
-
-    <br>
 
         <div class="col-md-3">
-    <a class="button" href="<?php echo site_url('Welcome/loadContactenLijstOpvragen/uri') ?>">
-        <span>Contact lijst opvragen</span>
-    </a>
-
+            <p><a class="button" href="<?php echo site_url('Welcome/loadContactenLijstOpvragen/uri') ?>">
+                    <span>Contact lijst opvragen</span>
+                </a></p>
         </div>
-    <br>
-            <div class="col-md-3">
-    <a class="button" href="<?php echo site_url('Welcome/loadContactenWijzigen/uri') ?>">
-        <span>Contact wijzigen</span>
-            </div>
-        <br>
-    </a>
 
-    <br>
         <div class="col-md-3">
-    <a class="button" href="<?php echo site_url('Welcome/loadAboutUs/uri') ?>">
-        <span>About us</span>
-    </a>
+            <p><a class="button" href="<?php echo site_url('Welcome/loadContactenWijzigen/uri') ?>">
+                    <span>Contact wijzigen</span>
+                </a></p>
         </div>
-</div>
-</div>
+
+        <div class="col-md-3">
+            <p><a class="button" href="<?php echo site_url('Welcome/loadAboutUs/uri') ?>">
+                    <span>About us</span>
+                </a></p>
+        </div>
+
+    </div>
 </div>
 </body>
 </html>
