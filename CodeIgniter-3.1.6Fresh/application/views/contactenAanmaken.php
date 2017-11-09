777ee4a8c38b99f980ce61292a1e1ff6f7769422
hannahpatronoudis <hannah.patronoudis@student.pxl.be>
2017-11-09 11:51:47 +0100

    CSS aanpassen
    Css Contact aanmaken aangemaakt

--- CodeIgniter-3.1.6Fresh/application/views/contactenAanmaken.php
+++ CodeIgniter-3.1.6Fresh/application/views/contactenAanmaken.php
@@ -5,17 +5,56 @@
     <title>Maak contact aan</title>
 </head>
 <body>
-<?php echo form_open('Welcome/addContacts'); ?>
-<?php echo form_label('ID :'); ?><br/>
-<?php echo form_input(array('id' => 'ID', 'name' => 'ID')); ?><br />
 
-<?php echo form_label('Email :'); ?><br />
-<?php echo form_input(array('id' => 'email', 'name' => 'email')); ?><br />
+</body>
+</html>
 
-<?php echo form_label('Name'); ?><br />
-<?php echo form_input(array('id' => 'name', 'name' => 'name')); ?>
-<br />
-<?php echo form_submit(array('id' => 'submit', 'value' => 'Submit')); ?>
-<?php echo form_close(); ?><br/>
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body class="w3-content" style="max-width:1300px">
+
+<div class="w3-row" id="contact">
+    <div class="w3-half w3-teal w3-container" style="height:700px">
+        <div class="w3-padding-64 w3-padding-large">
+            <h1>Welcome</h1>
+            <p class="w3-opacity">Add contact</p>
+            <form class="w3-container w3-card w3-padding-32 w3-white" action="/action_page.php" target="_blank">
+                <?php echo form_open('Welcome/addContacts'); ?>
+                <div class="w3-section">
+                    <label><?php echo form_label('ID :'); ?></label>
+                    <input class="w3-input"
+                           style="width:100%;"><?php echo form_input(array('id' => 'ID', 'name' => 'ID')); ?>
+                    <br/></input>
+                </div>
+                <div class="w3-section">
+                    <label><?php echo form_label('Email :'); ?></label>
+                    <input class="w3-input"
+                           style="width:100%;"><?php echo form_input(array('id' => 'email', 'name' => 'email')); ?>
+                    <br/></input>
+                </div>
+                <div class="w3-section">
+                    <label><?php echo form_label('Name'); ?></label>
+                    <input class="w3-input"
+                           style="width:100%;"><?php echo form_input(array('id' => 'name', 'name' => 'name')); ?>
+                    <br/></input>
+                </div>
+                <div class="w3-section">
+                    <label><?php echo form_submit(array('id' => 'submit', 'value' => 'Submit')); ?></label>
+                    <input class="w3-input" style="width:100%;"><?php echo form_close(); ?><br/></input>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-black w3-padding-16">
+    <p>Powered by Team 5</a></p>
+</footer>
+
 </body>
 </html>
--- CodeIgniter-3.1.6Fresh/application/views/welcome_message.php
+++ CodeIgniter-3.1.6Fresh/application/views/welcome_message.php
@@ -8,9 +8,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 
     <link href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css">
@@ -21,9 +22,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <p><a class="button" href="<?php echo site_url('Welcome/loadContactenAanmaken/uri') ?>">
-                    <span>Contact aanmaken</span>
-                </a></p>
+            <a class="button" role="button" href="<?php echo site_url('Welcome/loadContactenAanmaken/uri') ?>">
+                   <span> Contact aanmaken</span>
+                </a>
         </div>
 
         <div class="col-md-3">
