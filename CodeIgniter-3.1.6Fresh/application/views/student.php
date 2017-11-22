cdcb472b9654a7adf42c1e8942d70506c6b2a282
laurens1337 <laurens.bynens@hotmail.com>
2017-11-22 02:55:15 +0100

    WP3 ophalen/toevoegen met rest API van het internet omdat WP2 niet klaar is

--- CodeIgniter-3.1.6Fresh/application/views/student.php
+++ CodeIgniter-3.1.6Fresh/application/views/student.php
@@ -2,6 +2,7 @@
     <?php echo form_open('Welcome/editData','class="form-inline"') ?>
     <div style="margin-left:30%;margin-top:10%;">
 
+
         <div class="form-group">
             <label>Enter ID : </label>
             <input type="text" class="form-control" name="ID">
